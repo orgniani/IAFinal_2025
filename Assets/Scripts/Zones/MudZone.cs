16e6ecfad77fac76b138a976a20d2a2edb8c250c
lapiedranumerodos <maria.orgniani@estudiante.imagecampus.edu.ar>
2025-11-11 20:41:15 -0300

    fix mud zone

--- Assets/Scripts/Zones/MudZone.cs
+++ Assets/Scripts/Zones/MudZone.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,20 +8,26 @@ namespace Zones
     {
         [SerializeField] private float speedReductionPerc = 0.5f;
 
-        void OnTriggerEnter (Collider other)
-        {
-            NavMeshAgent agent = other.gameObject.GetComponentInParent<NavMeshAgent>();
+        private readonly Dictionary<NavMeshAgent, float> _originalSpeeds = new();
 
-            if (agent)
-                agent.speed *= speedReductionPerc;
+        private void OnTriggerEnter(Collider other)
+        {
+            var agent = other.GetComponent<NavMeshAgent>();
+            if (!agent || _originalSpeeds.ContainsKey(agent)) return;
+
+            _originalSpeeds[agent] = agent.speed;
+            agent.speed *= speedReductionPerc;
+            Debug.Log($"[MudZone] Reduced speed of {other.name} to {agent.speed}");
         }
-        
-        void OnTriggerExit (Collider other)
-        {
-            NavMeshAgent agent = other.gameObject.GetComponentInParent<NavMeshAgent>();
 
-            if (agent)
-                agent.speed /= speedReductionPerc;
+        private void OnTriggerExit(Collider other)
+        {
+            var agent = other.GetComponent<NavMeshAgent>();
+            if (!agent || !_originalSpeeds.ContainsKey(agent)) return;
+
+            agent.speed = _originalSpeeds[agent];
+            _originalSpeeds.Remove(agent);
+            Debug.Log($"[MudZone] Restored speed of {other.name} to {agent.speed}");
         }
     }
 }
